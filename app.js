ec025be4adfe7fce25c18ad6c9da3abf77af0349
PedroPimpao <pedrohmpimpao@gmail.com>
2025-06-24 18:58:32 -0300

    Rotas

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+app.get("/", (req, res)=>{
+    res.send("Seja bem vindo ao nosso site")
+})
+
+app.get("/artigos/:id", (req, res)=>{
+    if(req.params.id == '1'){
+        res.send('1 - Como criar aplicativos Android e IOS')
+    }else if(req.params.id == '2'){
+        res.send('2 - Como usar o Node.Js')
+    }else{
+        res.send('Não encontrado...')
+    }
+})
+
+app.get("/contato", (req, res)=>{
+    res.send('Deixe sua mensagem...')
+})
+
 app.listen(8081, ()=>{
     console.log('O servidor está rodando...')
 })
