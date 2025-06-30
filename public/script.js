bfe3515026261dcfbd712103349f6689cc3e1855
PedroPimpao <pedrohmpimpao@gmail.com>
2025-06-29 21:53:54 -0300

    CRUD - Carregando TODOS os produtos

--- public/script.js
+++ public/script.js
@@ -5,6 +5,38 @@ const findNameButton = document.querySelector('#btn-buscar-nome')
 const findIDButton = document.querySelector('#btn-buscar-id')
 const updateButton = document.querySelector('#btn-atualizar')
 const removeButton = document.querySelector('#btn-remover')
+const refreshButton = document.querySelector('#btn-recarregar')
+
+const productsList = document.querySelector('.products')
+const newProductElement = (product) =>{
+
+    const productData={
+        nome: product.nome,
+        preco: product.preco,
+        descricao: product.descricao
+    }
+
+    const productDiv = document.createElement('div')
+    productDiv.classList.add('product')
+
+    const nomeDiv = document.createElement('p')
+    nomeDiv.classList.add('nomeProduto')
+    nomeDiv.textContent = `Nome: ${productData.nome}`
+
+    const precoDiv = document.createElement('p')
+    precoDiv.classList.add('precoProduto')
+    precoDiv.textContent = `Preço: ${productData.preco}`
+
+    const descricaoDiv = document.createElement('p')
+    descricaoDiv.classList.add('descProduto')
+    descricaoDiv.textContent = `Descrição: ${productData.descricao}`
+
+    productDiv.appendChild(nomeDiv);
+    productDiv.appendChild(precoDiv);
+    productDiv.appendChild(descricaoDiv);
+
+    return productDiv
+}
 
 const createProduct=(evt)=>{
     evt.preventDefault()
@@ -24,14 +56,44 @@ const createProduct=(evt)=>{
         headers: {'Content-Type' : 'application/json'},
         body: JSON.stringify(data)
     })
-    .then(data => data.text())
+    .then(resp => resp.text())
     .then(result => console.log(result))
     .catch((erro)=>{
         console.log(`Não foi possível cadastrar o produto: ${erro}`)
     })
 }
 
+const findAllProducts=()=>{
+    fetch(URL)
+        .then(resp => resp.json())
+        .then((products)=>{
+            products.forEach((product)=>{
+                productsList.append(newProductElement(product))
+            })
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 createButton.addEventListener('click', createProduct)
+refreshButton.addEventListener('click', findAllProducts)
 
 
 
